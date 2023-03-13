60621681566ea6390b67e773bce8bbc9b558a6ae
dainank <whelbe@outlook.com>
2023-03-13 10:13:57 +0100

    vuetify v-app in root app.vue

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,19 @@
-<script setup lang="ts">
-</script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
 
+// reactive state
+const count = ref(0)
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
 <template>
-  <main>
-    <v-slider direction="vertical"></v-slider>
-  </main>
+    <v-row justify="center" class="mt-2">
+      <v-col sm="6">
+        <v-slider></v-slider>
+      </v-col>
+    </v-row>
 </template>
+<style></style>
