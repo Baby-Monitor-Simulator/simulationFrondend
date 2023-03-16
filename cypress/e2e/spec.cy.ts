b636d0cf6a424845d1d4eb69e5f1cc8c229c1763
dainank <dainank@proton.me>
2023-03-16 15:24:30 +0100

    basic e2e test, migrated map.txt data to fixtures folder

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,10 @@
+// import testDataTxt from "../fixtures/map.txt"
+
+describe('Test File Input', () => {
+  it('Upload File & Parse Data', () => {
+    cy.visit("http://127.0.0.1:5173/")
+    cy.get('#input-1').click();
+    cy.get('[type="file"]').selectFile('cypress/fixtures/map.txt')
+    cy.get('.mdi-cloud-upload').click();
+  })
+})
