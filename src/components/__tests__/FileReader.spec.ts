ba2a5eb0003a6f652bc6d423d40135df1ca98084
dainank <dainank@proton.me>
2023-03-16 14:20:37 +0100

    store unit tests conflict

---
+++ src/components/__tests__/FileReader.spec.ts
@@ -0,0 +1,25 @@
+import { createPinia, setActivePinia } from 'pinia'
+import { beforeEach, describe, expect, test, vitest } from 'vitest'
+import { useImportStore } from '@/stores/import'
+import FileReader from '@/components/FileReader.vue'
+import { createTestingPinia } from '@pinia/testing'
+import { mount } from '@vue/test-utils'
+
+describe('FileReader', () => {
+    beforeEach(() => {
+        const wrapper = mount(FileReader, {
+            global: {
+                plugins: [createTestingPinia({
+                    createSpy: vitest.fn
+                })],
+            },
+        })
+        setActivePinia(createPinia())
+    })
+
+    test('that the fileContent starts with an empty array', () => {
+        const store = useImportStore()
+
+        expect(store.fileContent).toStrictEqual([])
+    })
+})
