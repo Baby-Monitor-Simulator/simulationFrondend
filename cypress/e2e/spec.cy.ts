cc64ea954e92f7bce80a8eac59b14718eae87ed5
dainank <dainank@proton.me>
2023-03-16 16:18:20 +0100

    visit adjustment

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Test File Input', () => {
   it('Upload File & Parse Data', () => {
-    cy.visit("http://127.0.0.1:4173/")
+    cy.visit("http://localhost:4173/")
     cy.get('#input-1').click();
     cy.get('[type="file"]').selectFile('cypress/fixtures/map.txt')
     cy.get('.mdi-cloud-upload').click();
