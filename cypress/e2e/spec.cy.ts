86980d1f53d5c88bc1e0b50be4ad0021daac8d69
dainank <dainank@proton.me>
2023-03-23 09:31:39 +0100

    clarification msg for reviewers

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
 describe('Test File Input', () => {
   it('Upload File & Parse Data', () => {
+    // TODO: component not showing in e2e tests, temp disable but needs to be fixed asap
     cy.visit("http://localhost:4173/")
     // cy.get('#input-1').click();
     // cy.get('[type="file"]').selectFile('cypress/fixtures/map.txt')
