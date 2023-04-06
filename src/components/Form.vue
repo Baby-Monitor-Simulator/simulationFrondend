45ea624b554ca4ffb8fcbe2581f4f54f025664cd
dainank <whelbe@outlook.com>
2023-04-06 11:18:44 +0100

    cleanup form, still needs styling

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,30 +1,32 @@
 <template>
-    <form @submit.prevent="submit">
+    <v-form @submit.prevent="submit">
         <v-select variant="outlined" v-model="scenario.value.value" :items="scenarios"
             :error-messages="scenario.errorMessage.value" label="Selected Scenario"></v-select>
 
+        <v-select variant="outlined" v-model="umbilicalOption.value.value" :items="umbilicalOptions"
+            :error-messages="umbilicalOption.errorMessage.value" label="Umbilical Options"></v-select>
+
         <v-text-field variant="outlined" v-model.number="maxAmplitude.value.value"
-            :error-messages="maxAmplitude.errorMessage.value" type="text" label="Max Amplitude" placeholder="70"
+            :error-messages="maxAmplitude.errorMessage.value" label="Max Amplitude" placeholder="70"
             suffix="mmHg"></v-text-field>
 
         <v-text-field variant="outlined" v-model.number="contractionDuration.value.value"
-            :error-messages="contractionDuration.errorMessage.value" type="text" label="Contraction Duration" placeholder="60"
+            :error-messages="contractionDuration.errorMessage.value" label="Contraction Duration" placeholder="60"
             suffix="s"></v-text-field>
 
-        <v-text-field variant="outlined" v-model="nCycleMax.value.value" :counter="10"
-            :error-messages="nCycleMax.errorMessage.value" label="NCycleMax"></v-text-field>
+        <v-text-field variant="outlined" v-model.number="timeBetweenContractions.value.value"
+            :error-messages="timeBetweenContractions.errorMessage.value" label="Time Between Contractions" placeholder="180"
+            suffix="s"></v-text-field>
 
-        <v-text-field variant="outlined" v-model="bloodVolumeFetus.value.value" :counter="7"
-            :error-messages="bloodVolumeFetus.errorMessage.value" label="Blood Volume Fetus"></v-text-field>
+        <v-text-field variant="outlined" v-model.number="nCycleMax.value.value"
+            :error-messages="nCycleMax.errorMessage.value" label="NCycleMax" placeholder="??"></v-text-field>
 
-        <v-text-field variant="outlined" v-model="extraOxygen.value.value" :error-messages="extraOxygen.errorMessage.value"
-            label="Extra Oxygen"></v-text-field>
+        <v-text-field variant="outlined" v-model.number="bloodVolumeFetus.value.value"
+            :error-messages="bloodVolumeFetus.errorMessage.value" label="Blood Volume Fetus"
+            placeholder="330"></v-text-field>
 
-        <v-select variant="outlined" v-model="select.value.value" :items="umbilicalOptions"
-            :error-messages="select.errorMessage.value" label="Umbilical Options"></v-select>
-
-        <v-checkbox variant="outlined" v-model="checkbox.value.value" :error-messages="checkbox.errorMessage.value"
-            value="Option 1" label="Option" type="checkbox"></v-checkbox>
+        <v-text-field variant="outlined" v-model.number="extraOxygen.value.value"
+            :error-messages="extraOxygen.errorMessage.value" label="Extra Oxygen"  placeholder="??"></v-text-field>
 
         <v-btn variant="outlined" class="me-4" type="submit">
             submit
@@ -33,7 +35,7 @@
         <v-btn variant="outlined" @click="handleReset">
             clear
         </v-btn>
-    </form>
+    </v-form>
 </template>
 
 <script lang="ts">
@@ -54,23 +56,10 @@ export default {
                 contractionDuration(value: number) {
                     return value > 0 ? true : 'Contraction duration must be larger than 0.'
                 },
-
-                nCycleMax(value: string) {
-                    if (value?.length >= 2) return true
-
-                    return 'NCycleMax needs to be at least 2 characters.'
+                timeBetweenContractions(value: number) {
+                    return value > 0 ? true : 'Time between contractions must be larger than 0.'
                 },
-                bloodVolumeFetus(value: string) {
-                    if (value?.length > 9 && /[0-9-]+/.test(value)) return true
-
-                    return 'Blood volume (fetus) needs to be at least 9 digits.'
-                },
-                extraOxygen(value: string) {
-                    if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
-
-                    return 'Must be a valid e-mail.'
-                },
-                select(value: string) {
+                umbilicalOption(value: string) {
                     if (value) return true
 
                     return 'Must select an umbilical option.'
@@ -86,11 +75,12 @@ export default {
         const scenario = useField('scenario')
         const maxAmplitude = useField('maxAmplitude') // mmHg
         const contractionDuration = useField('contractionDuration') // seconds
+        const timeBetweenContractions = useField('timeBetweenContractions') // seconds
 
         const nCycleMax = useField('nCycleMax') // heartbeats of mother measured
         const bloodVolumeFetus = useField('bloodVolumeFetus')
         const extraOxygen = useField('extraOxygen') // can be done in oxygen4 as scenario>4
-        const select = useField('select') // umbilical options; 2 makes more calculations
+        const umbilicalOption = useField('umbilicalOption') // umbilical options; 2 makes more calculations
         const checkbox = useField('checkbox')
 
         const scenarios = ref([
@@ -109,7 +99,7 @@ export default {
             alert(JSON.stringify(values, null, 2))
         })
 
-        return { scenario, scenarios, contractionDuration, maxAmplitude, nCycleMax, bloodVolumeFetus, extraOxygen, select, checkbox, umbilicalOptions, submit, handleReset }
+        return { scenario, scenarios, contractionDuration, timeBetweenContractions, maxAmplitude, umbilicalOption, nCycleMax, bloodVolumeFetus, extraOxygen, checkbox, umbilicalOptions, submit, handleReset }
     },
 }
 </script>
