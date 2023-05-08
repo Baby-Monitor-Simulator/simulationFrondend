ae5b0f4889cb100c8c93a008bd34ecf361731d6a
dainank <dainank@proton.me>
2023-05-08 09:47:43 +0200

    typo

--- src/repositories/MatLabRepository.ts
+++ src/repositories/MatLabRepository.ts
@@ -2,7 +2,7 @@ import { Repository } from "@/repositories/Repository"
 import type ExampleInterface from "@/interfaces/ExampleInterface"
 
 export default class MatLabRepository extends Repository {
-    //get graph data from bbackend
+    // get graph data from backend
     async getGraphData(): Promise<ExampleInterface[]> {
         const response = await this.client().get(this.getUrl("v1/matlab/example"))
 
