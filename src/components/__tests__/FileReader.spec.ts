74ff7b06695a179aaac6dbd37cd0a9f3d0ca3660
dainank <whelbe@outlook.com>
2023-06-05 11:12:43 +0200

    i think this fixed it lmao

--- src/components/__tests__/FileReader.spec.ts
+++ src/components/__tests__/FileReader.spec.ts
@@ -4,15 +4,24 @@ import { useImportStore } from '@/stores/import'
 import FileReader from '@/components/FileReader.vue'
 import { createTestingPinia } from '@pinia/testing'
 import { mount } from '@vue/test-utils'
-import { useI18n } from 'vue-i18n'
+import { createI18n } from 'vue-i18n'
+import messages from '@/i18n/index'
 
 describe('FileReader', () => {
     beforeEach(() => {
+        const i18n = createI18n({
+            legacy: false, // you must set `false`, to use Composition API
+            locale: 'en', // set locale
+            fallbackLocale: 'en', // set fallback locale
+            messages, // set locale messages
+            globalInjection: true,
+        })
+
         const wrapper = mount(FileReader, {
             global: {
                 plugins: [createTestingPinia({
                     createSpy: vitest.fn
-                })],
+                }), i18n],
             },
         })
         setActivePinia(createPinia())
@@ -20,7 +29,7 @@ describe('FileReader', () => {
 
     test('that the fileContent starts with an empty array', () => {
         const store = useImportStore()
-        const { t } = useI18n() // call `useI18n`, and spread `t` from  `useI18n` returning
+        // const { t } = useI18n() // call `useI18n`, and spread `t` from  `useI18n` returning
 
         expect(store.graphData).toStrictEqual([])
     })
