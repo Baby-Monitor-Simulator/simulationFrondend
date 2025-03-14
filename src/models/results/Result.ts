806351b1542fe40d796b318fe9e010a2a3dd4b2e
Popodil <popodil@bornready.eu>
2025-03-14 15:31:42 +0100

    Added js docstrings

--- src/models/results/Result.ts
+++ src/models/results/Result.ts
@@ -1,7 +1,16 @@
+/**
+ * Base class for all result types.
+ * Provides common properties and functionality for result objects.
+ * @property {number} timeSpan - The time span of the result in milliseconds.
+ */
 export default class Result {
-
     timeSpan: number;
 
+    /**
+     * Creates a new Result instance.
+     * 
+     * @param {number} timeSpan - The time span of the result in milliseconds.
+     */
     constructor(timeSpan: number) {
         this.timeSpan = timeSpan;
     }
