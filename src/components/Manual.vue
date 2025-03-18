5046a41c79a389ae711885664b2c6d146b92b1e9
Popodil <popodil@bornready.eu>
2025-03-18 10:36:37 +0100

    Manual according to language file

--- src/components/Manual.vue
+++ src/components/Manual.vue
@@ -5,7 +5,6 @@ import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 
 const hoverStore: any = useHoverStore();
-const { t } = useI18n(); // call `useI18n`, and spread `t` from  `useI18n` returning
 
 // you can probably use `onmousemove` for both: https://www.w3schools.com/jsref/event_onmouseover.asp
 const changeHover = (item: HoverItem, bool: boolean) => {
@@ -25,22 +24,19 @@ const changeHover = (item: HoverItem, bool: boolean) => {
   }
 };
 
-const titleStrs = computed(() => {
+const { t } = useI18n();
+const languageStrs = computed(() => {
   return {
-    manualTitle: t("message.manualTitle"),
-    hoverExplanation: t("message.hoverExplanation"),
-
-    Languagechange: t("message.languageChangeTitle"),
-    LanguageChangeDescription: t("message.languageChangeDescription"),
-
-    dataExportTitle: t("message.dataExportTitle"),
-    dataExportDescription: t("message.dataExportDescription"),
-
-    dataImportTitle: t("message.dataImportTitle"),
-    dataImportDescription: t("message.dataImportDescription"),
-
-    submitTitle: t("message.submitTitle"),
-    submitDescription: t("message.submitDescription"),
+    manualTitle: t("header.manualTitle"),
+    hoverExplanation: t("header.hoverExplanation"),
+    languageChangeTitle: t("header.languageChangeTitle"),
+    languageChangeDescription: t("header.languageChangeDescription"),
+    dataExportTitle: t("header.dataExportTitle"),
+    dataExportDescription: t("header.dataExportDescription"),
+    dataImportTitle: t("header.dataImportTitle"),
+    dataImportDescription: t("header.dataImportDescription"),
+    submitTitle: t("header.submitTitle"),
+    submitDescription: t("header.submitDescription"),
   };
 });
 </script>
@@ -50,17 +46,19 @@ const titleStrs = computed(() => {
     <v-card class="mx-auto" max-width="425">
       <v-list lines="two">
         <v-list-subheader
-          ><b>{{ titleStrs.manualTitle }}</b></v-list-subheader
+          ><b>{{ languageStrs.manualTitle }}</b></v-list-subheader
         >
-        <v-list-subheader>{{ titleStrs.hoverExplanation }}</v-list-subheader>
+        <v-list-subheader>{{ languageStrs.hoverExplanation }}</v-list-subheader>
 
         <v-list-item
           v-on:mouseenter="changeHover(HoverItem.Language, true)"
           v-on:mouseleave="changeHover(HoverItem.Language, false)"
           prepend-icon="mdi-web"
         >
-          <template v-slot:title>{{ titleStrs.Languagechange }}</template>
-          <template v-slot:subtitle>{{ titleStrs.LanguageChangeDescription }} </template>
+          <template v-slot:title>{{ languageStrs.languageChangeTitle }}</template>
+          <template v-slot:subtitle
+            >{{ languageStrs.languageChangeDescription }}
+          </template>
         </v-list-item>
 
         <v-divider inset></v-divider>
@@ -70,8 +68,8 @@ const titleStrs = computed(() => {
           v-on:mouseleave="changeHover(HoverItem.Export, false)"
           prepend-icon="mdi-download"
         >
-          <template v-slot:title>{{ titleStrs.dataExportTitle }}</template>
-          <template v-slot:subtitle>{{ titleStrs.dataExportDescription }} </template>
+          <template v-slot:title>{{ languageStrs.dataExportTitle }}</template>
+          <template v-slot:subtitle>{{ languageStrs.dataExportDescription }} </template>
         </v-list-item>
 
         <v-divider inset></v-divider>
@@ -81,8 +79,8 @@ const titleStrs = computed(() => {
           v-on:mouseleave="changeHover(HoverItem.Import, false)"
           prepend-icon="mdi-upload"
         >
-          <template v-slot:title> {{ titleStrs.dataImportTitle }} </template>
-          <template v-slot:subtitle> {{ titleStrs.dataImportDescription }} </template>
+          <template v-slot:title> {{ languageStrs.dataImportTitle }} </template>
+          <template v-slot:subtitle> {{ languageStrs.dataImportDescription }} </template>
         </v-list-item>
 
         <v-divider inset></v-divider>
@@ -92,8 +90,8 @@ const titleStrs = computed(() => {
           v-on:mouseleave="changeHover(HoverItem.Submit, false)"
           prepend-icon="mdi-send"
         >
-          <template v-slot:title> {{ titleStrs.submitTitle }} </template>
-          <template v-slot:subtitle> {{ titleStrs.submitDescription }} </template>
+          <template v-slot:title> {{ languageStrs.submitTitle }} </template>
+          <template v-slot:subtitle> {{ languageStrs.submitDescription }} </template>
         </v-list-item>
       </v-list>
     </v-card>
