fea920d5d2e2d068ca2ffbac972eaf37a95be4a6
Andreas <89839661+Andreasd21@users.noreply.github.com>
2025-04-08 10:52:03 +0200

    MJS and dependicies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from 'node:path'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueI18nPlugin({
+      /* options */
+      // locale messages resource pre-compile option
       include: resolve(dirname(fileURLToPath(import.meta.url)), './path/to/src/locales/**'),
     }),
   ],
@@ -15,12 +19,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    extensions: ['.mjs', '.js', '.ts', '.vue', '.json'], // Ensure .mjs is recognized
+    extensions: ['.mjs', '.js', '.ts', '.vue', '.json'],
   },
-  optimizeDeps: {
-    include: ['vue-i18n'],
+  server: {
+    port: 4173,
+    headers: {
+      "Cache-Control": "no-store", // Voorkomt caching van bestanden
+    },
   },
   define: {
-    global: {}, 
+    global: {},
   },
-});
+})
