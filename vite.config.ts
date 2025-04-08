8fe75f7682a2553dea2862dd35a17131c9be833b
Andreas <89839661+Andreasd21@users.noreply.github.com>
2025-04-08 10:36:46 +0200

    Desparation

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from 'node:path'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueI18nPlugin({
-      /* options */
-      // locale messages resource pre-compile option
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './path/to/src/locales/**'),
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'), // Ensure correct path
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'], // Ensure Rollup handles .mjs
   },
   server: {
     port: 4173,
     headers: {
-      "Cache-Control": "no-store", // Voorkomt caching van bestanden
+      "Cache-Control": "no-store",
     },
   },
   define: {
