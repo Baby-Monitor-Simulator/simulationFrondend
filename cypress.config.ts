409b5fd59acf6643a7b1f772ab80866b7fce6f6e
Andreas <89839661+Andreasd21@users.noreply.github.com>
2025-04-11 13:45:21 +0200

    Fixed env

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
     baseUrl: "http://localhost:4173/",
     experimentalStudio: true,
     env: {
-      EMAIL: "Adminbm@example.com",
-      PASSWORD: "myadminpassword"
+      EMAIL: "testadmin@example.com",
+      PASSWORD: "adminpassword"
     }
   },
 
